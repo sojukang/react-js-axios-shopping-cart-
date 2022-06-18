00ae9f3be551027d59402064eb80b6898437bbe4
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-06-19 03:19:34 +0900

    feat: About QueryParam 추가

--- src/About.js
+++ src/About.js
@@ -1,11 +1,16 @@
 import React from "react";
+import {useSearchParams} from "react-router-dom";
 
 const About = () => {
+    const [searchParams] = useSearchParams();
+    const detail = searchParams.get('detail') === 'true';
+
     return (
         <div>
             <h1>About</h1>
             <p>You could borrow items from woowacourse crews.
                 you could freely register,delete and let crews borrow your items.</p>
+            {detail && <p>Special Information for true query</p>}
         </div>
     )
 }
