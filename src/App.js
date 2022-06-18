8ce081644b2ba242cfb9d8ef0ab6f4c70261c15f
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-06-19 02:45:34 +0900

    feat: index, about 페이지 링크 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import './App.css';
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
 const App = () => {
     return (
         <div>
+            <ul>
+                <li>
+                    <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                    <Link to={"/about"}>About</Link>
+                </li>
+            </ul>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
